f4579899673455d56879744986f6b8f10207bfa9
Srishti Srivastava <ssrishti508@gmail.com>
2024-01-05 00:02:00 +0530

    backdrop image for movies

--- js/script.js
+++ js/script.js
@@ -83,7 +83,10 @@ async function displayPopularShows() {
 async function displayMovieDetails() {
     const num = window.location.search.split('=')[1];
     const movie = await fetchdata(`movie/${num}`);
-    console.log(movie);
+
+    //over lay for background image
+
+    displayBackgroundImage('movie',movie.backdrop_path);
 
     const div = document.createElement('div');
 
@@ -142,6 +145,25 @@ async function displayMovieDetails() {
 
     document.querySelector('#movie-details').appendChild(div);
 
+}
+
+function displayBackgroundImage(type, backgroundPath) {
+    const overlayDiv = document.createElement('div');
+    overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+    overlayDiv.style.backgroundSize = 'cover';
+    overlayDiv.style.backgroundPosition = 'center';
+    overlayDiv.style.backgroundRepeat = 'no-repeat';
+    overlayDiv.style.height = '100vh';
+    overlayDiv.style.width = '100vw';
+    overlayDiv.style.position = 'absolute';
+    overlayDiv.style.top = '0';
+    overlayDiv.style.left = '0';
+    overlayDiv.style.zIndex = '-1';
+    overlayDiv.style.opacity = '0.1';
+
+    if(type=='movie'){
+        document.querySelector('#movie-details').appendChild(overlayDiv);
+    }
 }
 
 
